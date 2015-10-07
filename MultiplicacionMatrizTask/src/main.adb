a21f0cd08901b792f45bf258e03b684508a0d47c
iccarmandoduran <iccarmandoduran@hotmail.com>
2015-10-07 10:10:46 -0500

    proyectoFinal

--- MultiplicacionMatrizTask/src/main.adb
+++ MultiplicacionMatrizTask/src/main.adb
@@ -1,15 +1,17 @@
-with Ada.Text_IO ,Ada.Integer_Text_IO;
-use Ada.Text_IO , Ada.Integer_Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 procedure Main is
+   fin:Integer:=350;
+   MatrizA : array (1..fin,1..fin) of Integer;
+   MatrizB : array (1..fin,1..fin) of Integer;
+   MatrizC : array (1..fin,1..fin) of Integer;
+   r,i,j,k: Integer:=0;
 
-   MatrizA : array (1..5,1..5) of Integer;
-   MatrizB : array (1..5,1..5) of Integer;
-   MatrizC : array (1..5,1..5) of Integer;
-   r,i,j,k: Integer  := 0;
+
    task Llenado;
 
    task type Multiplicacion is
-      entry iniciar (inicio,fin:Integer);
+      entry iniciar (inicio:Integer);
    end Multiplicacion;
 
    task MatrizR is
@@ -18,57 +20,60 @@ procedure Main is
 
 
    task body Multiplicacion is
+      sumaR:Integer:=0;
    begin
-      accept iniciar (inicio: Integer; fin: Integer) do
-         for k in inicio..fin loop
+      accept iniciar (inicio: Integer) do
+         k:=inicio;
           for i in 1..fin loop
             r:=0;
             for j in 1..fin loop
                 r:= MatrizA(k,j)+MatrizB(j,i)+r;
                end loop;
                MatrizR.posicion(k,i,r);
-          end loop;
          end loop;
       end iniciar;
    end Multiplicacion;
 
-   SubMatrices : array (0..4) of Multiplicacion;
+
+
 
    task body MatrizR is
+      resultado : Integer:=0;
    begin
-      for i in 0..4 loop
-       accept posicion (c,f, val:Integer) do
-         MatrizC(c,f):=val;
-       end posicion;
+      for i in 1..fin loop
+         for j in 1..fin loop
+          accept posicion (c,f, val:Integer) do
+           MatrizC(c,f):=val;
+          end posicion;
+         end loop;
       end loop;
 
-      Put_Line("Multiplicación:");
-      for i in 1..4 loop
-       for j in 1..4 loop
-         Put(MatrizC(i,j));
+      for i in 1..fin loop
+       for j in 1..fin loop
+          Put(MatrizC(i,j));
        end loop;
        Put_Line("");
       end loop;
    end MatrizR;
 
-
+   SubMatrices : array (1..fin) of Multiplicacion;
 
    task body Llenado is
     begin
-     for i in 1..4 loop
-      for j in 1..4 loop
+     for i in 1..fin loop
+      for j in 1..fin loop
          MatrizA (i,j):= 2;
          MatrizB (i,j):= 2;
          end loop;
+      end loop;
+
+      for i in 1..fin loop
+         SubMatrices(i).iniciar(i);
       end loop;
    end Llenado;
 
 
 
 begin
-   Put_Line("Multiplicaciòn:");
-   for i in 0..4 loop
-         SubMatrices(i).iniciar(i,4);
-   end loop;
-   null;
+   Put_Line("Multiplicaciion Matrices");
 end Main;
